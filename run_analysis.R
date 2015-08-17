59d5f170381965094d88e8753a63470b984f16b5
Daren Welsh <darenwelsh@gmail.com>
2015-08-17 17:01:55 -0500

    init CodeBook and script

---
+++ run_analysis.R
@@ -0,0 +1,17 @@
+run_analysis <- function(){
+    #
+    # This script will do the following:
+    # 1. Merge training & test data sets into one data set
+    # 2. Extract only mean & sd for each measurement
+    # 3. Name activities in data set with descriptive activity names
+    # 4. Label data set with descriptive variable names
+    # 5. From #4, create new data set with mean of each variable for each 
+    #    activity and each subject
+    # 
+    # For more detailed information,
+    # see https://github.com/darenwelsh/Coursera-GettingCleaningData-Project
+    # 
+    
+    
+    
+}
