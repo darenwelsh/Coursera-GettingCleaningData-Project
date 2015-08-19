0bf6afcc6fd1bd6b353f27318d06075bf015eecf
Daren Welsh <darenwelsh@gmail.com>
2015-08-19 11:55:04 -0500

    tidy data through step 4 complete

--- run_analysis.R
+++ run_analysis.R
@@ -13,11 +13,12 @@ run_analysis <- function(){
     # 
     # For more detailed information,
     # see https://github.com/darenwelsh/Coursera-GettingCleaningData-Project
+    # which includes a ReadMe and CodeBook.
     # 
     
     ## setwd("~/r/Coursera/Coursera-GettingCleaningData-Project")
     
-    # read in names for the 561 variables
+    # read in given names for the 561 variables
     features <- read.table("./dataset/features.txt")
     var_names <- as.character(features[,2])
 
@@ -34,25 +35,56 @@ run_analysis <- function(){
     activity <- rbind(test_y, train_y)
     activity <- as.data.frame(activity)
     names(activity) <- c("activity")
+    # change activity labels to useful words based on activity_labels.txt
+    activity[activity=="1"] <- "Walking"
+    activity[activity=="2"] <- "Walking Upstairs"
+    activity[activity=="3"] <- "Walking Downstairs"
+    activity[activity=="4"] <- "Sitting"
+    activity[activity=="5"] <- "Standing"
+    activity[activity=="6"] <- "Laying" # (should be Lying)
     
-    # read in the 561 variables
+    # read in the 561 variable data
     test_x <- read.table("./dataset/test/X_test.txt")
     train_x <- read.table("./dataset/train/X_train.txt")
     datapoints <- rbind(test_x, train_x)
     datapoints <- as.data.frame(datapoints)
     names(datapoints) <- var_names
     
-    # subset from 561 variables to just means and SDs
-    sub_var_list_mean <- agrep("mean", var_names, value=TRUE)
-    sub_var_list_std <- agrep("std", var_names, value=TRUE)
+    # subset from 561 variables to just mean()s and std()s
+    # explain in code book why I chose just mean/std of measurements
+    # not every mean in the dataset
+    sub_var_list_mean <- agrep("mean()", var_names, value=TRUE)
+    sub_var_list_std <- agrep("std()", var_names, value=TRUE)
     sub_var_list <- append(sub_var_list_mean, sub_var_list_std)
-    
+    # downselect from 561 to just the variables related to mean or std
     datapoints_subset <- datapoints[,sub_var_list]
-    
+    # modify variable names using info from features_info.txt
+    # from "t" to "time"
+    names(datapoints_subset) <- gsub("tBody","timeBody", names(datapoints_subset))
+    names(datapoints_subset) <- gsub("tGravity","timeGravity", names(datapoints_subset))
+    # from "f" to "frequency"
+    names(datapoints_subset) <- gsub("fBody","frequencyBody", names(datapoints_subset))
+    names(datapoints_subset) <- gsub("fGravity","frequencyGravity", names(datapoints_subset))
+    # from "Acc" to "Accelerometer"
+    names(datapoints_subset) <- gsub("Acc","Accelerometer", names(datapoints_subset))
+    # from "Gyro" to "Gyroscope"
+    names(datapoints_subset) <- gsub("Gyro","Gyroscope", names(datapoints_subset))
+    # from "Mag" to "Magnitude"
+    names(datapoints_subset) <- gsub("Mag","Magnitude", names(datapoints_subset))
+    # remove "()" after "mean" in strings with "mean()" at end
+    names(datapoints_subset) <- gsub("mean.[^-]*$","Mean", names(datapoints_subset))
+    # remove "()" after "mean" in strings with "mean()-"
+    names(datapoints_subset) <- gsub("mean..*-","Mean-", names(datapoints_subset))
+    # remove "()" after "std" in strings with "std()" at end
+    names(datapoints_subset) <- gsub("std.[^-]*$","StandardDeviation", names(datapoints_subset))
+    # remove "()" after "std" in strings with "std()-"
+    names(datapoints_subset) <- gsub("std..*-","StandardDeviation-", names(datapoints_subset))
     
     # bind into one df
     test_df <- cbind(subject, activity, datapoints_subset)
+
     
-    summary(test_df)
+    print(names(test_df))
+    print(head(test_df[,1:3]))
     
 }
